55cfc47a767b589119fff9888f364dfc998ef2d8
Lorenzo Baldassari <lorebalda93@gmail.com>
2023-09-04 15:20:41 +0200

    removed some comments

--- assets/D1.js
+++ assets/D1.js
@@ -24,42 +24,42 @@ Ci sono 5 principali tipi di datatype:
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/*let myName = "lorenzo";
-console.log("myName = ", myName);*/
+let myName = "lorenzo";
+console.log("myName = ", myName);
 
 /* ESERCIZIO 3 
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/*let nu1 = 12 + 20;
-console.log("nu1 = ", nu1);*/
+let nu1 = 12 + 20;
+console.log("nu1 = ", nu1);
 /*con una sola variable*/
 
-/*let num1, num2, num3;
+let num1, num2, num3;
 num1 = 12;
 num2 = 20;
 num3 = num1 + num2;
-console.log("num3 = ", num3); */
+console.log("num3 = ", num3);
 /* con variabili per ogni valore*/
 
-/*console.log("12+20 = ", 12 + 20);*/
+console.log("12+20 = ", 12 + 20);
 /* direttamente dalla console*/
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
-/*let x = 12;
-console.log("x = ", x); */
+let x = 12;
+console.log("x = ", x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/*myName = "Baldassari";
+myName = "Baldassari";
 console.log("myName = ", myName);
 const fixedNumber1 = 4;
-console.log("fixedNumber1 = ", fixedNumber1); */
+console.log("fixedNumber1 = ", fixedNumber1);
 /*fixedNumber1 = 10;
 console.log(fixedNumber1); */
 
@@ -67,8 +67,8 @@ console.log(fixedNumber1); */
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/*console.log("4-x = ", 4 - x);
-console.log("x-4 = ", x - 4);*/
+console.log("4-x = ", 4 - x);
+console.log("x-4 = ", x - 4);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -76,11 +76,11 @@ console.log("x-4 = ", x - 4);*/
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/*let name1 = "john",
+let name1 = "john",
   name2 = "John";
 console.log("name1 e`", name1, " name2 e`", name2);
 console.log("name1 e` uguale a name2?", name1 === name2);
 console.log(
   "name1 e` uguale a name2 utilizzando lowercase?",
-  name1.toLowerCase() === name2.toLowerCase() 
-);*/
+  name1.toLowerCase() === name2.toLowerCase()
+);
